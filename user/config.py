76ae38deed537769315ca991e9546bd152dc83e6
xiangwenbin <tmyyss@163.com>
2020-06-18 10:56:09 +0800

    修改mongodb和flask相关配置

--- user/config.py
+++ user/config.py
@@ -3,22 +3,18 @@
 Use env var to override
 """
 
-from environs import Env
+import os
 
-env = Env()
-
-env.read_env(".flaskenv", recurse=False)
-
-ENV = env("FLASK_ENV", "production")
+ENV = os.environ.get("FLASK_ENV", "production")
 DEBUG = ENV == "development"
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DATABASE_URI = env("DATABASE_URI")
+DATABASE_URI = os.environ.get("DATABASE_URI")
 
 JWT_BLACKLIST_ENABLED = True
 JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
-CELERY_BROKER_URL = env("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND_URL")
-DEFAULT_LOG_DIR = env("DEFAULT_LOG_DIR", "./logs")
-DEFAULT_LOG_FILE = env("DEFAULT_LOG_FILE", "default.log")
-ELASTICSEARCH_URL = env("ELASTICSEARCH_URL")
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND_URL")
+DEFAULT_LOG_DIR = os.environ.get("DEFAULT_LOG_DIR", "./logs")
+DEFAULT_LOG_FILE = os.environ.get("DEFAULT_LOG_FILE", "default.log")
+ELASTICSEARCH_URL = os.environ.get("ELASTICSEARCH_URL")
