238bcd0d3764f919751b1b881471ed84a0b98ff4
xiangwb <xiangwenbin521@gmail.com>
2020-06-29 17:02:42 +0800

    1、修改http response返回数据结构

--- user/request_handler.py
+++ user/request_handler.py
@@ -17,6 +17,7 @@ import traceback
 from flask import jsonify, request
 
 from user.extensions import logger
+from utils.response import format_response
 
 
 def register_error_handler(app):
@@ -31,81 +32,101 @@ def register_error_handler(app):
 
 def handle_400(e):
     """捕获abort(400)错误"""
-    rsp = {
-        'msg': 'Bad Request',
-        'err': 10
-    }
-    if isinstance(e.description, int):
-        rsp['err'] = e.description
-    if isinstance(e.description, dict):
-        rsp.update(e.description)
-    return jsonify(**rsp), 400
+    traceback.print_exc()
+    logger.api_logger.error(traceback.format_exc())
+    # rsp = {
+    #     'msg': 'Bad Request',
+    #     'err': 10
+    # }
+    # if isinstance(e.description, int):
+    #     rsp['err'] = e.description
+    # if isinstance(e.description, dict):
+    #     rsp.update(e.description)
+    # # return jsonify(**rsp), 400
+    return format_response(str(e), 'bad request', 400)
 
 
 def handle_401(e):
     """捕获abort(401)错误"""
-    rsp = {
-        'msg': 'Unauthorized',
-        'err': 20
-    }
-    if isinstance(e.description, int):
-        rsp['err'] = e.description
-    elif isinstance(e.description, dict):
-        rsp.update(e.description)
-    return jsonify(**rsp), 401
+    traceback.print_exc()
+    logger.api_logger.error(traceback.format_exc())
+    # rsp = {
+    #     'msg': 'Unauthorized',
+    #     'err': 20
+    # }
+    # if isinstance(e.description, int):
+    #     rsp['err'] = e.description
+    # elif isinstance(e.description, dict):
+    #     rsp.update(e.description)
+    # return jsonify(**rsp), 401
+    return format_response(str(e), 'Unauthorized', 401)
 
 
 def handle_403(e):
     """捕获abort(403)错误"""
-    rsp = {
-        'err': 400,
-        'msg': 'Forbidden'
-    }
-    if isinstance(e.description, int):
-        rsp['err'] = e.description
-    if isinstance(e.description, dict):
-        rsp.update(e.description)
-    return jsonify(**rsp), 403
+    traceback.print_exc()
+    logger.api_logger.error(traceback.format_exc())
+    # rsp = {
+    #     'err': 400,
+    #     'msg': 'Forbidden'
+    # }
+    # if isinstance(e.description, int):
+    #     rsp['err'] = e.description
+    # if isinstance(e.description, dict):
+    #     rsp.update(e.description)
+    # return jsonify(**rsp), 403
+    return format_response(str(e), 'Forbidden', 403)
 
 
 def handle_404(e):
     """捕获abort(404)错误"""
-    rsp = {
-        'msg': 'Not Found',
-        'err': 50
-    }
-    if isinstance(e.description, dict):
-        rsp.update(e.description)
-    return jsonify(**rsp), 404
+    traceback.print_exc()
+    logger.api_logger.error(traceback.format_exc())
+    # rsp = {
+    #     'msg': 'Not Found',
+    #     'err': 50
+    # }
+    # if isinstance(e.description, dict):
+    #     rsp.update(e.description)
+    # return jsonify(**rsp), 404
+    return format_response(str(e), 'Not Found', 404)
+
 
 
 def handle_405(e):
     """捕获abort(405)错误"""
-    rsp = {
-        'msg': 'Method not allowed',
-        'err': 60
-    }
-    return jsonify(**rsp), 405
+    traceback.print_exc()
+    logger.api_logger.error(traceback.format_exc())
+    # rsp = {
+    #     'msg': 'Method not allowed',
+    #     'err': 60
+    # }
+    # return jsonify(**rsp), 405
+    return format_response(str(e), 'Method not allowed', 405)
 
 
 def handle_429(e):
     """捕获abort(429)频率限制错误"""
-    rsp = {
-        'msg': 'Too Many Requests',
-        'err': 30
-    }
-    err_info = {
-        'msg': 'Too many requests',
-        'ip': request.remote_addr,
-        'url': request.url,
-        'method': request.method,
-    }
-    logger.api_logger.error(err_info)
-    return jsonify(**rsp), 429
+    traceback.print_exc()
+    logger.api_logger.error(traceback.format_exc())
+    # rsp = {
+    #     'msg': 'Too Many Requests',
+    #     'err': 30
+    # }
+    # err_info = {
+    #     'msg': 'Too many requests',
+    #     'ip': request.remote_addr,
+    #     'url': request.url,
+    #     'method': request.method,
+    # }
+    # logger.api_logger.error(err_info)
+    # return jsonify(**rsp), 429
+    return format_response(str(e), 'Too many requests', 429)
 
 
 def handle_exception(e):
     """Called when exception occurred"""
     traceback.print_exc()
     logger.api_logger.error(traceback.format_exc())
-    return jsonify(str(e)), 500
+    # return jsonify(str(e)), 500
+    return format_response(str(e), 'server error', 429)
