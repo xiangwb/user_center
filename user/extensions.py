2f61846b251dc04f1ee2d2a9c2d56198e27d68ea
xiangwb <xiangwenbin521@gmail.com>
2020-07-08 13:49:12 +0800

    修改异常返回的状态码

--- user/extensions.py
+++ user/extensions.py
@@ -29,14 +29,14 @@ limiter = Limiter(key_func=get_remote_address, default_limits=["10000/day, 2000/
 
 @jwt.unauthorized_loader
 def my_unauthorized_loader(e):
-    return jsonify(format_response('', 'Missing Authorization Header', 401))
+    return jsonify(format_response('', 'Missing Authorization Header', 401)), 401
 
 
 @jwt.invalid_token_loader
 def my_invalid_token_loader(e):
-    return jsonify(format_response('', "Bad Authorization header. Expected value 'Bearer <JWT>'", 422))
+    return jsonify(format_response('', "Bad Authorization header. Expected value 'Bearer <JWT>'", 422)), 422
 
 
 @jwt.expired_token_loader
 def my_expired_token_loader(e):
-    return jsonify(format_response('', "Token has expired", 401))
+    return jsonify(format_response('', "Token has expired", 401)), 401
